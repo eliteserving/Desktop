2f8ec4ce8237c38e71af76a20b281151bdb17add
eliteserving <erimrank167@gmail.com>
2026-07-12 17:27:33 +0300

    Updates

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const { app, BrowserWindow } = require("electron");
 const path = require("path");
-
 let mainWindow;
-
 function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
@@ -10,15 +8,13 @@ function createWindow() {
     minWidth: 800,
     minHeight: 600,
     autoHideMenuBar: true,
-    icon: path.join(__dirname, "public", "icon.png"),
+    icon: path.join(__dirname, "public", "icon.jpeg"),
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
     },
   });
-
   mainWindow.loadFile(path.join(__dirname, "public", "index.html"));
-
   mainWindow.webContents.on("before-input-event", (event, input) => {
     if ((input.control || input.meta) && input.key.toLowerCase() === "p") {
       event.preventDefault();
@@ -26,17 +22,14 @@ function createWindow() {
     }
   });
 }
-
 app.whenReady().then(() => {
   createWindow();
-
   app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow();
     }
   });
 });
-
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
     app.quit();
